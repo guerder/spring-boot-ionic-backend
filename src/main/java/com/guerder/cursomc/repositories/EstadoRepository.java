8468decd2b71057be5476dc296a007d97f695d53
Guerder Vieira <g7.vieira@gmail.com>
2019-12-20 22:47:33 -0300

    Estado e cidade

---
+++ src/main/java/com/guerder/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.guerder.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guerder.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+}
