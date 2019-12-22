f1d145d9035951ac17987648bebff1a2dbe4b66d
Guerder Vieira <g7.vieira@gmail.com>
2019-12-22 00:54:29 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/guerder/cursomc/CursomcApplication.java
+++ src/main/java/com/guerder/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.guerder.cursomc.domain.Cidade;
 import com.guerder.cursomc.domain.Cliente;
 import com.guerder.cursomc.domain.Endereco;
 import com.guerder.cursomc.domain.Estado;
+import com.guerder.cursomc.domain.ItemPedido;
 import com.guerder.cursomc.domain.Pagamento;
 import com.guerder.cursomc.domain.PagamentoComBoleto;
 import com.guerder.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.guerder.cursomc.repositories.CidadeRepository;
 import com.guerder.cursomc.repositories.ClienteRepository;
 import com.guerder.cursomc.repositories.EnderecoRepository;
 import com.guerder.cursomc.repositories.EstadoRepository;
+import com.guerder.cursomc.repositories.ItemPedidoRepository;
 import com.guerder.cursomc.repositories.PagamentoRepository;
 import com.guerder.cursomc.repositories.PedidoRepository;
 import com.guerder.cursomc.repositories.ProdutoRepository;
@@ -34,27 +36,22 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
-	
 	@Autowired
 	ProdutoRepository produtoRepository;
-	
 	@Autowired
 	EstadoRepository estadoRepository;
-	
 	@Autowired
 	CidadeRepository cidadeRepository;
-	
 	@Autowired
 	ClienteRepository clienteRepository;
-	
 	@Autowired
 	EnderecoRepository enderecoRepository;
-	
 	@Autowired
 	PedidoRepository pedidoRepository;
-	
 	@Autowired
 	PagamentoRepository pagamentoRepository;
+	@Autowired
+	ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -118,5 +115,18 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pgto1, pgto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
---
+++ src/main/java/com/guerder/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.guerder.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guerder.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+	
+}
