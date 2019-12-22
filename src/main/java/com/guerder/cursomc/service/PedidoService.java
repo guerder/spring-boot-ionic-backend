4dc45ce026601e70b606561408f654e68ffe7ab9
Guerder Vieira <g7.vieira@gmail.com>
2019-12-22 13:59:06 -0300

    Endpoint /pedidos/{id} disponível

---
+++ src/main/java/com/guerder/cursomc/service/PedidoService.java
@@ -0,0 +1,23 @@
+package com.guerder.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guerder.cursomc.domain.Pedido;
+import com.guerder.cursomc.repositories.PedidoRepository;
+import com.guerder.cursomc.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		}
+}
